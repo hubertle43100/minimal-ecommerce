cf57b091db14cccd5d03d41ec73adef48109d03d
RevealSquid <hubertle43100@gmail.com>
2023-12-21 13:39:17 -0800

    Change sanity to minimal to ignore file for deployment

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
 // import AddToBag from "@/app/components/AddToBag";
 // import CheckoutNow from "@/app/components/CheckoutNow";
 import AddToBag from "@/app/components/AddToBag";
+import CheckoutNow from "@/app/components/CheckoutNow";
 import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
@@ -83,7 +84,7 @@ export default async function ProductPge({
                 key={data._id}
                 price_id={data.price_id}
               />
-              {/* <CheckoutNow
+              <CheckoutNow
                 currency="USD"
                 description={data.description}
                 image={data.images[0]}
@@ -91,7 +92,7 @@ export default async function ProductPge({
                 price={data.price}
                 key={data._id}
                 price_id={data.price_id}
-              /> */}
+              />
             </div>
 
             <p className="mt-12 text-base text-gray-500 tracking-wide">
