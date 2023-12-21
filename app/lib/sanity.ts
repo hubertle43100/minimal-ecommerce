c589e5b8b9f5340493045a8475b25503edc12750
RevealSquid <hubertle43100@gmail.com>
2023-12-21 13:27:57 -0800

    finished project

---
+++ app/lib/sanity.ts
@@ -0,0 +1,16 @@
+//Fetch Sanity Data (important)
+import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = createClient({
+  projectId: "ug0zeu7i",
+  dataset: "production",
+  apiVersion: "2022-03-25",
+  useCdn: true,
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
